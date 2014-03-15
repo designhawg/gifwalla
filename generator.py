a117c372836574fc70f2833fcfc9e4c9a73109ea
designhawg <designhawg@mac.com>
2014-03-14 23:30:57 -0500

    made further adjustments to date

--- generator.py
+++ generator.py
@@ -29,6 +29,7 @@ class Post(object):
 				content += line
 		self.__dict__.update(yaml.load(content))
 
+@app.template_filter('date')
 def format_date(value, format='%B %d, %Y'):
 	return value.strftime(format)
 
